1ea3d088f0fe44c7e8e08ebf1c6d922c181f518f
Agustin Somoza Polchi <agustin.somoza@patagoniadreams.com.ar>
2026-06-02 16:51:21 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
     >
       <body className="h-[100dvh] flex flex-col overflow-hidden bg-[#030711] text-white max-w-md mx-auto dark:bg-black py-12">
         <Header />
-        <div className="flex min-h-0 flex-1 flex-col px-4 py-4">
+        <div className="flex min-h-0 flex-1 flex-col mx-4 py-4">
           {children}
           <ServiceWorkerRegister />
         </div>
